c6f3649f9cc0cd5058ea0fdb0054f90ea5a8e97a
JAYESH-U <uramanattijayesh@gmail.com>
2022-08-08 00:01:55 +0530

    inheritance

---
+++ practice  and revision/inheritance_problem_by_CWH.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+/*
+create two classes 
+    1.simple calculator -> takes input of 2 numbers using a utility functions and perform four arithmatic operations(+,-,*,/).
+    2.scientific calculator -> takes input of 2 numbers using a utility function and perform any four scientific operation of your choice and display the results using another function.
+
+    create another class hybrid calculator and inherit it using these 2 classes:
+    q1. What type of inheritance are you using ?
+    q2. Which mode of inheritance are you using ?
+    q3. create an object of hybrid calculator and display results of simple and scientific calculator.
+    q4. How is code reusbility implimented?
+*/
+
+int main()
+{
+    return 0;
+}
