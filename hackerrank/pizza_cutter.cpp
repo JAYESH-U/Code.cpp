761828707d991ad4765f68c014a7f565592ac607
JAYESH-U <uramanattijayesh@gmail.com>
2022-10-22 07:18:15 +0530

    format

--- hackerrank/pizza_cutter.cpp
+++ hackerrank/pizza_cutter.cpp
@@ -1,40 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int t;
-    int*n;
+    int *n;
     int **d;
-    cin>>t;
-    n = new int [t];
+    int *c;
+    cin >> t;
+    n = new int[t];
     d = new int *[t];
-    
-    for(int i = 0;i <t;i++)
-    {
-        cin>>n[i];
-        d[i] = new int [n[i]];
-        for (int j = 0;j < n[i]; j++)
-        {
-            cin>>d[i][j];
-            if(d[i][j]>360)
-            {
+    c = new int[t];
 
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n[i];
+        d[i] = new int[n[i]];
+        for (int j = 0; j < n[i]; j++)
+        {
+            c[i] = 0;
+            cin >> d[i][j];
+            if (d[i][j] > 360)
+            {
+                while (d[i][j] > 360)
+                {
+                    d[i][j] -= 360;
+                }
             }
         }
     }
 
-/*    
-    cout<<endl;
-    for(int i = 0;i <t;i++)
-    {
-        cout<<n[i];
-        for (int j = 0;j < n[i]; j++)
-            cout<<d[i][j];
+    /*
         cout<<endl;
-    }
-*/
+        for(int i = 0;i <t;i++)
+        {
+            cout<<n[i];
+            for (int j = 0;j < n[i]; j++)
+                cout<<d[i][j];
+            cout<<endl;
+        }
+    */
 
-    
     return 0;
 }
