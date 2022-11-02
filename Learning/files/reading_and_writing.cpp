0d51bf43151f3614412209cd37a830195a65be52
JAYESH-U <uramanattijayesh@gmail.com>
2022-11-02 22:39:57 +0530

    file read

--- Learning/files/reading_and_writing.cpp
+++ Learning/files/reading_and_writing.cpp
@@ -6,24 +6,32 @@ using namespace std;
 
 void read()
 {
-    string st2;
+    system("CLS");
+    string st2,st1;
     // Opening files using constructor and reading it
-    ifstream in("sample60b.txt"); // Read operation
-    //in>>st2; //reads only one word.
-    getline(in, st2);  
-    cout<<st2;
+    ifstream in,in1;
+    in.open("sample60.txt");    // Read operation
+    //in>>st1; //reads only one word.
+    getline(in,st1);
+    cout<<st1<<endl;
+    in.close();
+
+    in1.open("sample60b.txt");
+    getline(in1, st2);  
+    cout<<st2<<endl;
+    in1.close();
 }
 
 int main(){
-    string st = "Jayesh bhai";
+    string st = "This is the first file.";
     // Opening files using constructor and writing it
     // Write operation
     ofstream out("sample60.txt"); 
     out<<st;
-    //create a file before executind this program , and 2lines below are useless,
-    //because it will make a file with output stream and we cannot open the file with input stream when it is already opened
-    // ofstream out1("sample60b.txt"); //write operation 2
-    // out1<<"This is the second file.";
+    out.close();
+    ofstream out1("sample60b.txt"); //write operation 2
+    out1<<"This is the second file.";
+    out1.close();
     read();
     return 0;
 }
